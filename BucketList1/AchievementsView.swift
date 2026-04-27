fd9917e22980b75b199f439513c3806c474e9822
Jendayi <jendayianderson0@gmail.com>
2026-04-27 16:29:33 -0400

    badges and badges page

--- BucketList1/AchievementsView.swift
+++ BucketList1/AchievementsView.swift
@@ -6,88 +6,48 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct AchievementsView: View {
-//    @State var badge: Badge
-//    
-//    @State private var badges: [Badge] = [
-//        Badge(imageName: "B1", isUnlocked: true),
-//        Badge(imageName: "B2", isUnlocked: false),
-//        Badge(imageName: "B3", isUnlocked: false),
-//        Badge(imageName: "B4", isUnlocked: false),
-//        Badge(imageName: "B5", isUnlocked: false)
-//    ]
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var stickerImage: UIImage?
     
+    let stickers: [String] = [ "trophy", "rocket", "popcorn", "diamond", "pencil", "target", "flag", "notebook", "bulb", "mic", "spaceship", "meteor", "firecracker", "bomb", "fire", "gradcap" ]
+    let columns = [
+        GridItem(.adaptive(minimum: 180))
+    ]
     var body: some View {
-     
+        
         ScrollView(.vertical) {
-        VStack{
-            HStack{
+            VStack{
+                HStack{
+                    
+                    Image(systemName: "star.fill")
+                        .font(.system(size: 28))
+                        .foregroundStyle(.yellow)
+                        .rotationEffect(.degrees(60))
+                        .offset(y: -20)
+                    Image(systemName: "star.fill")
+                        .font(.system(size: 40))
+                        .foregroundStyle(.red)
+                        .rotationEffect(.degrees(90))
+                }.frame(maxWidth: .infinity, alignment: .trailing)
                 
-                Image(systemName: "star.fill")
-                    .font(.system(size: 28))
-                    .foregroundStyle(.pink)
-                    .rotationEffect(.degrees(90))
+                Text("Achievements")
+                    .foregroundColor(.red)
+                    .font(.custom("Soopafresh", size: 40))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .center)
                 
-                NavigationLink {
-                  HomeroomView()
-               } label: {
-                    Image(systemName: "folder.fill")
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(stickers, id: \.self) { sticker in
+                        Image(sticker)
+                            .resizable()
+                            .scaledToFit()
+                        
+                    }.padding()
                 }
-                
-                Image(systemName: "star.fill")
-                    .font(.system(size: 28))
-                    .foregroundStyle(.pink)
-                    .rotationEffect(.degrees(90))
-            }.frame(maxWidth: .infinity, alignment: .trailing)
-            
-            Text("Achievements")
-                .foregroundColor(.red)
-                .font(.custom("Soopafresh", size: 35))
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .center)
-            
-            HStack{
-                
-                Image("B1")
-                    .resizable()
-                    .scaledToFit()
-                Image("B8")
-                    .resizable()
-                    .scaledToFit()
-            }
-            HStack{
-                Image("B3")
-                    .resizable()
-                    .scaledToFit()
-                Image("B4")
-                    .resizable()
-                    .scaledToFit()
-            }
-            HStack{
-                Image("B5")
-                    .resizable()
-                    .scaledToFit()
-                Image("B6")
-                    .resizable()
-                    .scaledToFit()
-            }
-            HStack{
-                Image("B7")
-                    .resizable()
-                    .scaledToFit()
-                Image("B2")
-                    .resizable()
-                    .scaledToFit()
-            }
-            HStack{
-                Image("B9")
-                    .resizable()
-                    .scaledToFit()
-                Image("B10")
-                    .resizable()
-                    .scaledToFit()
-            }
             }
         }
     }
